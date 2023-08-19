1ea00400cecfab2aed711079ea12e221c10679c2
David Gómez <117203802+davidsgomez@users.noreply.github.com>
2023-08-18 20:23:59 -0600

    Add files via upload

---
+++ Ejercicios/Program2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ArrayEjercicio2;
+
+class Program
+{
+    static void Main()
+    {
+
+        string[] nombreCompleto = { "David", "Sibrian" };
+
+        foreach (string valor in nombreCompleto)
+        {
+            Console.WriteLine(valor);
+        }
+
+        int longitudArreglo = nombreCompleto.Length;
+
+        Console.WriteLine("Tamaño del arreglo: " + longitudArreglo);
+        foreach (string valor in nombreCompleto)
+        {
+            Console.WriteLine("Valor: " + valor);
+        }
+    }
+}
---
+++ Ejercicios/Program4.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ArrayEjercicio4;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] array = { "Perro", "Casa", "Cebra", "Tortuga", "Burro", "Mar", "Dedo", "Tasa", "Nube", "Sol" };
+
+        Console.WriteLine("posiciones inicializadas:");
+        for (int i = 0; i < array.Length; i++)
+        {
+            Console.WriteLine($"Posición {i}: {array[i]}");
+        }
+
+        Array.Sort(array);
+
+        Console.WriteLine("\nordenado:");
+        foreach (string elemento in array)
+        {
+            Console.WriteLine(elemento);
+        }
+
+        string[] palabrasBusqueda = { "Sol", "Dedo", "Casa" };
+        foreach (string palabra in palabrasBusqueda)
+        {
+            int posicion = Array.BinarySearch(array, palabra);
+
+            if (posicion >= 0)
+            {
+                Console.WriteLine($"\nLa palabra '{palabra}' se encuentra en la posición {posicion}.");
+            }
+            else
+            {
+                Console.WriteLine($"\nLa palabra '{palabra}' no se encontró en el array.");
+            }
+        }
+    }
+}
+
